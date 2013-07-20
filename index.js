c955e16eb23cb4bc6db7a70d8c54a37be6a40f73
Stephen Morrison <srmorrisonjit@gmail.com>
2013-07-20 13:02:36 -0400

    fixing bad merge

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
-var textParse = function() {
-  var self = {};
-
+module.exports = function(text) {
   // splits the text at \n
-<<<<<<< HEAD
-  self.textToParagraphs = function(text) {
-=======
-  var textToParagraphs = function (text) {
->>>>>>> parent of 41c9019... fixing typos and formating
+  var textToParagraphs = function(text) {
     var paragraphs = text.split( /[\r\n|\n|\r]+/g );
-    return paragraphs.map(self.paragraphToSentences);
+    return paragraphs.map(paragraphToSentences);
   };
 
   // splits the paragraph at end of sentence punctuation
-<<<<<<< HEAD
-  self.paragraphToSentences = function(paragraph) {
-=======
-  var paragraphToSentences = function (paragraph) {
->>>>>>> parent of 41c9019... fixing typos and formating
+  var paragraphToSentences = function(paragraph) {
     paragraph = paragraph.trim();
     var sentences = paragraph.match( /[^\.!\?]+[\.!\?(?="|')]+(\s|$)/g );
     return {
       raw: paragraph,
       type: 'paragraph',
-      children: sentences.map(self.sentenceToWords)
+      children: sentences.map(sentenceToWords)
     };
   };
 
   // splits the sentence at the spaces
-<<<<<<< HEAD
-  self.sentenceToWords = function(sentence) {
-=======
   var sentenceToWords = function (sentence) {
->>>>>>> parent of 41c9019... fixing typos and formating
     sentence = sentence.trim();
     var words = sentence.split(/\s+/);
     return {
       raw: sentence,
       type: 'sentence',
-      children: words.map(self.wordToChars)
+      children: words.map(wordToChars)
     };
   };
 
   // splits the word into characters and classifies each
-<<<<<<< HEAD
-  self.wordToChars = function(word) {
-=======
   var wordToChars = function (word) {
->>>>>>> parent of 41c9019... fixing typos and formating
     var chars = [];
     var letters = '';
     var punctuation = [];
@@ -74,10 +56,5 @@ var textParse = function() {
     };
   };
 
-  return self;
-};
-
-module.exports = function(text) {
-  var parse = textParse();
-  return parse.textToParagraphs(text);
+  return textToParagraphs(text);
 };
